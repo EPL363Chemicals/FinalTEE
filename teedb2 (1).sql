7b1e8291d6ee4f881f30f461556edf8f561c2bfc
akkous01 <akkous01@cs.ucy.ac.cy>
2015-05-05 15:30:29 +0300

    new db and icons

--- teedb2 (1).sql
+++ teedb2 (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2015 at 11:39 AM
+-- Generation Time: May 05, 2015 at 02:29 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -42,6 +42,13 @@ CREATE TABLE IF NOT EXISTS `company` (
   `Country` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`Company_Name`, `Lequl_State`, `Address`, `PostCode`, `City`, `Telephone`, `Fax`, `Email`, `info`, `Commersial_Name`, `Other_Name`, `CEO`, `Country`) VALUES
+('Κουρκουτας ΛΤΔ', 'LTD', 'Μηχαλάκη Καραολή 36', 2130, 'Λευκωσία', 225639871, 0, '', '96583334', 'Courkoutas LTD', 'Αλλο', 'Ειρήνη Παπακώστα', 'BR');
+
 -- --------------------------------------------------------
 
 --
@@ -60,6 +67,14 @@ CREATE TABLE IF NOT EXISTS `comperson` (
   `City` varchar(15) NOT NULL,
   `type` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comperson`
+--
+
+INSERT INTO `comperson` (`Name`, `Company`, `Surname`, `Address`, `Telephone`, `Fax`, `Email`, `PostCode`, `City`, `type`) VALUES
+('Άντρια', 'Κουρκουτας ΛΤΔ', 'Κκουσιή', 'Φώτης Πίττα 55', 96583334, 96583334, 'akkous01@cs.ucy.ac.cy', 7103, 'Λάρνακα', 0),
+('Μαρία', NULL, 'Παναγιώτου', 'Μακαρίου 7', 963258741, 123654789, 'maria@gmail.com', 1234, 'Λευκωσία', 1);
 
 -- --------------------------------------------------------
 
@@ -95,9 +110,41 @@ CREATE TABLE IF NOT EXISTS `product` (
   `Final` bit(1) NOT NULL,
   `Append` bit(1) NOT NULL,
   `Valid` bit(1) NOT NULL,
-  `Sds` varchar(1000) DEFAULT NULL,
-  `Clp` varchar(1000) DEFAULT NULL
+  `Sds` varchar(1000) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `producth`
+--
+
+CREATE TABLE IF NOT EXISTS `producth` (
+  `productName` varchar(30) NOT NULL,
+  `hid` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `producticon`
+--
+
+CREATE TABLE IF NOT EXISTS `producticon` (
+  `ProductName` varchar(30) NOT NULL,
+  `iconPath` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productp`
+--
+
+CREATE TABLE IF NOT EXISTS `productp` (
+  `ProductName` varchar(30) NOT NULL,
+  `pid` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -111,9 +158,41 @@ CREATE TABLE IF NOT EXISTS `substance` (
   `Ec` varchar(9) DEFAULT NULL,
   `Iupac` varchar(255) DEFAULT NULL,
   `Other_Name` varchar(50) DEFAULT NULL,
-  `Clp` varchar(100) DEFAULT NULL,
   `Sort` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `substanceh`
+--
+
+CREATE TABLE IF NOT EXISTS `substanceh` (
+  `CAS` varchar(12) NOT NULL,
+  `Hid` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `substanceicon`
+--
+
+CREATE TABLE IF NOT EXISTS `substanceicon` (
+  `CAS` varchar(12) NOT NULL,
+  `iconId` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `substancep`
+--
+
+CREATE TABLE IF NOT EXISTS `substancep` (
+  `CAS` varchar(12) NOT NULL,
+  `PID` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -153,6 +232,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Password` varchar(8) NOT NULL,
   `Comperson` int(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Username`, `Password`, `Comperson`) VALUES
+('akkous01', '123123', 96583334),
+('maria01', '123123', 963258741);
 
 -- --------------------------------------------------------
 
@@ -195,10 +282,46 @@ ALTER TABLE `product`
  ADD PRIMARY KEY (`Commercial_Name`,`SavingDate`);
 
 --
+-- Indexes for table `producth`
+--
+ALTER TABLE `producth`
+ ADD PRIMARY KEY (`productName`,`hid`);
+
+--
+-- Indexes for table `producticon`
+--
+ALTER TABLE `producticon`
+ ADD PRIMARY KEY (`ProductName`,`iconPath`);
+
+--
+-- Indexes for table `productp`
+--
+ALTER TABLE `productp`
+ ADD PRIMARY KEY (`ProductName`,`pid`);
+
+--
 -- Indexes for table `substance`
 --
 ALTER TABLE `substance`
  ADD PRIMARY KEY (`Cas`);
+
+--
+-- Indexes for table `substanceh`
+--
+ALTER TABLE `substanceh`
+ ADD PRIMARY KEY (`CAS`,`Hid`);
+
+--
+-- Indexes for table `substanceicon`
+--
+ALTER TABLE `substanceicon`
+ ADD PRIMARY KEY (`CAS`,`iconId`);
+
+--
+-- Indexes for table `substancep`
+--
+ALTER TABLE `substancep`
+ ADD PRIMARY KEY (`CAS`,`PID`);
 
 --
 -- Indexes for table `supply`
